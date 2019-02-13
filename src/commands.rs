14f99c2b1918fda2134b8e0abfde5494cfb1d0c3
Youssef Habri <youssef.habri@gmail.com>
2019-02-13 15:25:51 +0100

    Added `fortune` command

--- src/commands.rs
+++ src/commands.rs
@@ -2,12 +2,14 @@ use serenity::framework::StandardFramework;
 
 pub mod anilist;
 pub mod giphy;
+pub mod fortune;
 pub mod meta;
 pub mod nekoslife;
 pub mod urban;
 
 pub fn register(mut framework: StandardFramework) -> StandardFramework {
     framework = anilist::register(framework);
+    framework = fortune::register(framework);
     framework = nekoslife::register(framework);
     framework = urban::register(framework);
 
---
+++ src/commands/fortune.rs
@@ -0,0 +1,74 @@
+use serenity::prelude::*;
+use serenity::model::channel::Message;
+use serenity::framework::standard::{Args, Command, CommandError};
+use serenity::framework::StandardFramework;
+
+use crate::utils::random_num;
+
+pub fn register(framework: StandardFramework) -> StandardFramework {
+    framework.group("Fun", |cg| cg
+        .command("fortune", |c| c
+            .cmd(FortuneCommand)
+            .desc("Find out you fortune. It just might be you lucky day ...")
+        )
+    )
+}
+
+pub struct FortuneCommand;
+
+impl Command for FortuneCommand {
+    fn execute(&self, _: &mut Context, message: &Message, _: Args) -> Result<(), CommandError> {
+
+        match random_fortune() {
+            Some(fortune) => {
+                let _ = message.channel_id.send_message(|m| m
+                    .embed(|e| e
+                        .field(
+                            format!("{}'s fortune!", message.author.name),
+                            fortune.message,
+                            false
+                        )
+                    )
+                );
+            },
+            None => {
+                let _ = message.channel_id.say("Couldn't find any fortune for you. Sorry!");
+            }
+        };
+
+        Ok(())
+    }
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Fortune {
+    pub id: String,
+    pub message: String
+}
+
+fn request() -> Option<Vec<Fortune>> {
+    let client = reqwest::Client::new();
+    let page = random_num(0, 6);
+    let mut response = client.get(
+        format!("http://fortunecookieapi.herokuapp.com/v1/fortunes?limit=&skip=&page={}", page).as_str()
+    ).send().expect("fortune response");
+
+    let fortune_response: Vec<Fortune> = match response.json() {
+        Ok(res) => res,
+        Err(why) => {
+            error!("Err requesting fortunes: {:?}", why);
+            return None;
+        }
+    };
+
+    Some(fortune_response)
+}
+
+fn random_fortune() -> Option<Fortune> {
+    match request() {
+        Some(fortunes) => {
+            Some(fortunes.get(random_num(0, fortunes.len())).unwrap().clone())
+        },
+        None => None
+    }
+}
