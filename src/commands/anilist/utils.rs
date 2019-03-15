1a12163bd4f982b149bf8fb09cb1dfdb59107c9c
Youssef Habri <youssef.habri@gmail.com>
2019-03-15 20:32:54 +0100

    Hide spoilers in description

--- src/commands/anilist/utils.rs
+++ src/commands/anilist/utils.rs
@@ -1,27 +1,54 @@
 use dissolve::strip_html_tags;
 use math::round::floor;
+use regex::Regex;
+
+fn _remove_spoilers(content: &String) -> String {
+    let re = Regex::new(r"<span class='markdown_spoiler'>(.+?)</span>").unwrap();
+
+    let mut result = String::new();
+
+    for cap in re.captures_iter(content.as_str()) {
+        let capture = strip_html_tags(&cap[0]).join("");
+        result = content.replace(capture.as_str(), "");
+    }
+
+    result
+}
+
+fn clean_spoilers(content: String) -> String {
+    let content = content
+        .replace("~!", "||")
+        .replace("!~", "||");
+
+    let spoiler_pairs: Vec<_> = content.match_indices("||").collect();
+
+    let result = if spoiler_pairs.len() % 2 != 0 {
+        let index = spoiler_pairs[spoiler_pairs.len() - 1].0;
+        content.split_at(index).0.to_string()
+    } else {
+        content
+    };
+
+    result
+}
 
 pub fn synopsis(description: &String, length: usize) -> String {
-    let mut synopsis = strip_html_tags(description.as_str()).join(" ");
-    synopsis = synopsis.replace("\n\n", "\n");
 
-    let segments: Vec<&str> = synopsis.split("\n").collect();
-
-    let slices = if segments.len() > 5 {
-        &segments[..5]
-    } else {
-        segments.as_slice()
-    };
-    synopsis = slices.join("\n");
+    let synopsis = description.clone();
+//    synopsis = synopsis.replace("\n\n", "\n");
 
     if synopsis.len() > length {
         // Slicing by nth character rather and a simple index
         let end = synopsis.char_indices().map(|(i, _)| i).nth(length).unwrap();
-        // &synopsis.truncate(length);
-        return format!("{} ...", &synopsis[0..end]);
+
+        let mut result = (&synopsis[0..end]).to_string();
+        result = result.split_at(result.rfind(" ").unwrap()).0.to_string();
+        result = clean_spoilers(result);
+
+        return format!("{} ...", result);
     }
 
-    synopsis
+    if !synopsis.is_empty() { synopsis } else { "N/A".into() }
 }
 
 pub fn format_time(time_minutes: f64) -> String {
