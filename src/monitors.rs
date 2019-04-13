8c9d7b05f0e6d47c3482ff8b2d4045bdb272b03d
Youssef Habri <youssef.habri@gmail.com>
2019-04-13 23:44:08 +0100

    Disabled the rem monitor

--- src/monitors.rs
+++ src/monitors.rs
@@ -6,6 +6,6 @@ mod anilist;
 pub fn run_monitors(ctx: &Context, message: &Message) {
     if !message.author.bot {
         anilist::anilist_links_monitor(&ctx, &message);
-        anilist::rem_monitor(&ctx, &message);
+        //anilist::rem_monitor(&ctx, &message);
     }
 }
