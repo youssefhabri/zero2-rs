0be53332e57176303fd8440f3aea29497e114a86
Youssef Habri <youssef.habri@gmail.com>
2019-04-13 17:59:51 +0100

    Limit the stats command to once every 6 hours

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -7,18 +7,21 @@ mod ping;
 mod stats;
 
 pub fn register(framework: StandardFramework) -> StandardFramework {
-    framework.group("Meta", |cg| cg
-        .command("ping", |c| c
-            .cmd(ping::Ping)
-        )
-        .command("info", |c| c
-            .cmd(botinfo::BotInfo)
-        )
-        .command("stats", |c| c
-            .cmd(stats::Stats)
-        )
-        .command("avatar", |c| c
-            .cmd(avatar::Avatar)
-        )
+    framework
+        .simple_bucket("stats_limit", 6 * 3600)
+        .group("Meta", |cg| cg
+            .command("ping", |c| c
+                .cmd(ping::Ping)
+            )
+            .command("info", |c| c
+                .cmd(botinfo::BotInfo)
+            )
+            .command("stats", |c| c
+                .cmd(stats::Stats)
+                .bucket("stats_limit")
+            )
+            .command("avatar", |c| c
+                .cmd(avatar::Avatar)
+            )
     )
 }
