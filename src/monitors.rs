8fd4ab06842759354d24da60488c761a289ab637
Youssef Habri <youssef.habri@gmail.com>
2019-04-17 17:00:17 +0100

    Fixed monitors getting triggered when running a command

--- src/monitors.rs
+++ src/monitors.rs
@@ -3,9 +3,20 @@ use serenity::model::channel::Message;
 
 mod anilist;
 
+fn starts_with_prefix(message: &Message) -> bool {
+    let prefixes = dotenv::var("BOT_PREFIXES").expect("prefixes");
+    for prefix in prefixes.split(",") {
+        if message.content_safe().as_str().starts_with(prefix) {
+            return true;
+        }
+    }
+
+    false
+}
+
 pub fn run_monitors(ctx: &Context, message: &Message) {
-    if !message.author.bot {
-        anilist::anilist_links_monitor(&ctx, &message);
+    if !message.author.bot && !starts_with_prefix(message) {
+        anilist::anilist_links_monitor(ctx, message);
         //anilist::rem_monitor(&ctx, &message);
     }
 }
