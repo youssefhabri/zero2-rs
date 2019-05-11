e42ae49a4f74ac8a11925df12a0137f906bf9d3c
Youssef Habri <youssef.habri@gmail.com>
2019-05-11 17:44:58 +0000

    Add `bigtext` command

--- src/commands/fun.rs
+++ src/commands/fun.rs
@@ -1,12 +1,14 @@
 use serenity::framework::standard::macros::group;
 
+mod bigtext;
 mod golendar;
 mod fortune;
 
+use self::bigtext::BIGTEXT_COMMAND;
 use self::golendar::GOLENDAR_COMMAND;
 use self::fortune::FORTUNE_COMMAND;
 
 group!({
     name: "Fun",
-    commands: [golendar, fortune]
+    commands: [bigtext, golendar, fortune]
 });
---
+++ src/commands/fun/bigtext.rs
@@ -0,0 +1,52 @@
+use serenity::framework::standard::{macros::command, Args, CommandResult};
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+#[command("bigtext")]
+#[aliases("bt")]
+fn bigtext_command(context: &mut Context, message: &Message, args: Args) -> CommandResult {
+    let full_message: String = args.message().to_string();
+
+    if full_message.is_empty() {
+        let _ = message
+            .channel_id
+            .say(&context.http, "You need to input a message to convert");
+        return Ok(());
+    }
+
+    let bigtext_message: String = text_to_bigtext(full_message);
+
+    let _ = message.channel_id.say(&context.http, bigtext_message);
+
+    Ok(())
+}
+
+fn text_to_bigtext(text: String) -> String {
+    text.chars().map(|c| {
+        match c {
+            'a'...'z' | 'A'...'Z' => format!(":regional_indicator_{}:", c.to_lowercase()),
+            '0'...'9' => {
+                match c {
+                    '0' => ":zero:",
+                    '1' => ":one:",
+                    '2' => ":two:",
+                    '3' => ":three:",
+                    '4' => ":four:",
+                    '5' => ":five:",
+                    '6' => ":six:",
+                    '7' => ":seven:",
+                    '8' => ":eight:",
+                    '9' => ":nine:",
+                    _ => ""
+                }.to_string()
+            },
+            ' ' => "  ".to_string(),
+            '!' => ":exclamation:".to_string(),
+            '?' => ":question:".to_string(),
+            '+' => ":heavy_plus_sign:".to_string(),
+            '-' => ":heavy_minus_sign:".to_string(),
+            '.' => ":small_blue_diamond:".to_string(),
+            _ => c.to_string()
+        }
+    }).collect::<Vec<String>>().join("")
+}
