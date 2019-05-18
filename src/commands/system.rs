9c2cc9e9aa91b1fa59b62a651876fb9b4d4114d6
Youssef Habri <youssef.habri@gmail.com>
2019-05-18 23:49:17 +0000

    Add echo command

--- src/commands/system.rs
+++ src/commands/system.rs
@@ -1,4 +1,5 @@
-use crate::core::store::{Command, CommandLogger};
+use std::collections::HashMap;
+
 use serenity::framework::standard::{
     macros::{command, group},
     Args, CommandResult,
@@ -6,9 +7,10 @@ use serenity::framework::standard::{
 use serenity::model::channel::Message;
 use serenity::model::id::MessageId;
 use serenity::prelude::*;
-use std::collections::HashMap;
+use serenity::utils::parse_channel;
 
 use crate::checks::*;
+use crate::core::store::{Command, CommandLogger};
 
 #[command("log")]
 #[checks(Admin)]
@@ -35,7 +37,48 @@ fn log_command(context: &mut Context, message: &Message, _: Args) -> CommandResu
     Ok(())
 }
 
+#[command("echo")]
+#[owners_only]
+fn echo_command(context: &mut Context, message: &Message, args: Args) -> CommandResult {
+    if args.is_empty() {
+        let _ = message
+            .channel_id
+            .say(&context.http, "You need to input a channel & a message.");
+
+        return Ok(());
+    }
+
+    let segments: Vec<&str> = args.message().split(" ").collect();
+    let channel = parse_channel(segments[0]);
+
+    if let Some(channel_id) = parse_channel(segments[0]) {
+        if segments.len() == 1 {
+            message.channel_id.say(
+                &context.http,
+                format!("You need to type a message to send to <#{}>", channel_id),
+            )?;
+
+            return Ok(());
+        }
+
+        if segments.len() > 1 {
+            // Send message to channel
+            let message_text = segments[1..].join(" ");
+            message.channel_id.say(&context.http, message_text)?;
+
+            return Ok(());
+        }
+    } else {
+        if segments.len() >= 1 && channel.is_none() {
+            // send message to current channel
+            message.channel_id.say(&context.http, segments.join(" "))?;
+        }
+    }
+
+    Ok(())
+}
+
 group!({
     name: "System",
-    commands: [log]
+    commands: [echo, log]
 });
