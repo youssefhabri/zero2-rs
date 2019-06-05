d4c7ec485fd25697690338005d8cef199173add6
Youssef Habri <youssef.habri@gmail.com>
2019-06-05 11:24:58 +0000

    Moved `owo` command to the fun category

--- src/commands/fun.rs
+++ src/commands/fun.rs
@@ -3,12 +3,14 @@ use serenity::framework::standard::macros::group;
 mod bigtext;
 mod fortune;
 mod golendar;
+mod owo;
 
 use self::bigtext::BIGTEXT_COMMAND;
 use self::fortune::FORTUNE_COMMAND;
 use self::golendar::GOLENDAR_COMMAND;
+use self::owo::OWO_COMMAND;
 
 group!({
     name: "Fun",
-    commands: [bigtext, golendar, fortune]
+    commands: [bigtext, golendar, fortune, owo]
 });
--- src/commands/nekoslife.rs
+++ src/commands/nekoslife.rs
@@ -1,12 +1,10 @@
 use serenity::framework::standard::macros::group;
 
 pub mod nlimage;
-pub mod nlowo;
 
 use self::nlimage::NLIMAGE_COMMAND;
-use self::nlowo::NLOWO_COMMAND;
 
 group!({
     name: "NekosLife",
-    commands: [nlowo, nlimage]
+    commands: [nlimage]
 });
