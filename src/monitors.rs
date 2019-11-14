6d59de8c1f883653576b7bfb0081e7984a72eab0
Youssef Habri <youssef.habri@gmail.com>
2019-11-14 14:46:40 +0100

    Add support for embedding messages from different channels

--- src/monitors.rs
+++ src/monitors.rs
@@ -5,10 +5,7 @@ use serenity::prelude::Context;
 use crate::core::consts::PREFIX;
 
 mod anilist;
-
-lazy_static! {
-    static ref MSG_RE: Regex = Regex::new(r"[0-9]{17,18}").unwrap();
-}
+mod message_id;
 
 pub fn message_monitors(context: &Context, message: &Message) {
     if !message.author.bot
@@ -19,59 +16,10 @@ pub fn message_monitors(context: &Context, message: &Message) {
     {
         anilist::anilist_links_monitor(context, message);
         //anilist::rem_monitor(&context, &message);
-        message_id_monitor(context, message);
+        message_id::message_id_monitor(context, message);
     }
 }
 
 pub fn new_member_monitors(_context: &Context, _guild_id: GuildId, _new_member: &Member) {
     // Greet the user?
-}
-
-pub fn message_id_monitor(context: &Context, message: &Message) {
-    for cap in MSG_RE.find_iter(message.content.as_str()) {
-        if let Ok(msg_id) = cap.as_str().parse::<u64>() {
-            if let Ok(msg) = message.channel_id.message(context, msg_id) {
-                if !msg.content.is_empty() || !msg.attachments.is_empty() {
-                    let guild_id = match message.guild_id {
-                        Some(id) => id,
-                        None => return,
-                    };
-
-                    let url = format!(
-                        "[Jump!](https://discordapp.com/channels/{}/{}/{}/)",
-                        guild_id.as_u64(),
-                        msg.channel_id.as_u64(),
-                        msg.id.as_u64()
-                    );
-
-                    let _ = message.channel_id.send_message(context, |m| {
-                        m.embed(|e| {
-                            e.author(|a| {
-                                a.name(
-                                    msg.author
-                                        .nick_in(context, guild_id)
-                                        .unwrap_or_else(|| msg.author.name.clone()),
-                                )
-                                .icon_url(
-                                    msg.author
-                                        .avatar_url()
-                                        .unwrap_or_else(|| msg.author.default_avatar_url()),
-                                )
-                            })
-                            .description(msg.content)
-                            .field("Original", url, false);
-
-                            if !msg.attachments.is_empty()
-                                && msg.attachments[0].dimensions().is_some()
-                            {
-                                e.image(msg.attachments[0].url.clone());
-                            };
-
-                            e
-                        })
-                    });
-                }
-            }
-        }
-    }
 }
---
+++ src/monitors/message_id.rs
@@ -0,0 +1,88 @@
+use regex::Regex;
+use serenity::model::{
+    channel::Message,
+    id::{ChannelId, GuildId, MessageId},
+};
+use serenity::prelude::Context;
+
+use crate::core::consts;
+
+lazy_static! {
+    static ref MSG_RE: Regex = Regex::new(r"[0-9]{17,18}").unwrap();
+}
+
+fn message_url(guild_id: GuildId, channel_id: ChannelId, message_id: MessageId) -> String {
+    format!(
+        "[Jump!](https://discordapp.com/channels/{}/{}/{}/)",
+        guild_id, channel_id, message_id
+    )
+}
+
+pub fn message_id_monitor(context: &Context, message: &Message) {
+    let guild_id = match message.guild_id {
+        Some(id) => id,
+        None => return,
+    };
+
+    for cap in MSG_RE.find_iter(message.content.as_str()) {
+        if let Ok(msg_id) = cap.as_str().parse::<u64>() {
+            match message.channel_id.message(context, msg_id) {
+                Ok(msg) => handle_message(context, guild_id, message.channel_id, &msg),
+                Err(why) => {
+                    warn!("[MessageID Monitor] {}", why);
+                    if let Ok(channels) = guild_id.channels(context) {
+                        for (channel_id, _) in channels {
+                            match channel_id.message(context, msg_id) {
+                                Ok(msg) => {
+                                    handle_message(context, guild_id, message.channel_id, &msg);
+                                    break;
+                                }
+                                Err(why) => warn!("[MessageID Monitor] {}", why),
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn handle_message(
+    context: &Context,
+    guild_id: GuildId,
+    target_channel_id: ChannelId,
+    message: &Message,
+) {
+    if !message.content.is_empty() || !message.attachments.is_empty() {
+        let url = message_url(guild_id, message.channel_id, message.id);
+
+        let _ = target_channel_id.send_message(context, |m| {
+            m.embed(|e| {
+                e.author(|a| {
+                    a.name(
+                        message
+                            .author
+                            .nick_in(context, guild_id)
+                            .unwrap_or_else(|| message.author.name.clone()),
+                    )
+                    .icon_url(
+                        message
+                            .author
+                            .avatar_url()
+                            .unwrap_or_else(|| message.author.default_avatar_url()),
+                    )
+                })
+                .colour(consts::MAIN_COLOUR)
+                .description(message.content.clone())
+                .field("Original", url, false);
+
+                if !message.attachments.is_empty() && message.attachments[0].dimensions().is_some()
+                {
+                    e.image(message.attachments[0].url.clone());
+                };
+
+                e
+            })
+        });
+    }
+}
