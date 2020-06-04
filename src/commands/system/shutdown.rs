73032093e10e805a3550b3936c9e40c82224b6fb
Youssef Habri <youssef.habri@gmail.com>
2020-06-04 22:25:54 +0100

    Add a message to the shutdown command

--- src/commands/system/shutdown.rs
+++ src/commands/system/shutdown.rs
@@ -6,6 +6,11 @@ use std::process::exit;
 
 #[command]
 #[owners_only]
-fn shutdown(_: &mut Context, _: &Message, _: Args) -> CommandResult {
+fn shutdown(context: &mut Context, message: &Message, _: Args) -> CommandResult {
+    let _ = message.channel_id.say(
+        context,
+        "Shutting down ... This is not enough to silence me. I will come back. I promise ...",
+    );
+
     exit(0);
 }
