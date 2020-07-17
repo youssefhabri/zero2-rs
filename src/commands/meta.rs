a29aeac9ff429561a3a15c43cf4d7aa795937aca
Youssef Habri <youssef.habri@gmail.com>
2020-07-17 21:33:46 +0100

    Add the `who` command

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -3,11 +3,13 @@ use serenity::framework::standard::macros::group;
 mod avatar;
 mod botinfo;
 mod ping;
+mod who;
 
 use self::avatar::AVATAR_COMMAND;
 use self::botinfo::BOT_INFO_COMMAND;
 use self::ping::PING_COMMAND;
+use self::who::WHO_COMMAND;
 
 #[group]
-#[commands(avatar, bot_info, ping)]
+#[commands(avatar, bot_info, who, ping)]
 struct Meta;
---
+++ src/commands/meta/who.rs
@@ -0,0 +1,62 @@
+use serenity::framework::standard::{macros::command, Args, CommandError, CommandResult};
+use serenity::model::prelude::{Member, Message, UserId};
+use serenity::prelude::Context;
+use serenity::utils::{parse_mention, Colour};
+
+use crate::core::consts::MAIN_COLOUR;
+
+#[command]
+fn who(context: &mut Context, message: &Message, args: Args) -> CommandResult {
+    let user_id: UserId = user_id_from_message(message, args)?;
+
+    let member: Member = match message.guild_id {
+        Some(guild_id) => guild_id.member(&context, user_id)?,
+        None => return Err(CommandError::from("Unexpected Error! Seek help!")),
+    };
+
+    let colour = member.colour(&context).unwrap_or(Colour::new(MAIN_COLOUR));
+
+    let nick = member
+        .nick
+        .clone()
+        .unwrap_or(member.display_name().to_string());
+
+    let joined_date = match member.joined_at {
+        Some(date) => date.format("%a, %B %e, %Y at %H:%M:%S").to_string(),
+        None => "N/A".to_string(),
+    };
+
+    let roles = {
+        member
+            .roles
+            .into_iter()
+            .map(|role_id| format!("<@&{}>", role_id))
+            .collect::<Vec<String>>()
+            .join(" ")
+    };
+
+    let avatar_url = { member.user.read().face() };
+
+    let _ = message.channel_id.send_message(&context, |m| {
+        m.embed(|e| {
+            e.title(nick)
+                .colour(colour)
+                .thumbnail(avatar_url)
+                .field("Roles", roles, false)
+                .field("Joined", joined_date, false)
+        })
+    });
+
+    Ok(())
+}
+
+fn user_id_from_message(message: &Message, mut args: Args) -> Result<UserId, CommandError> {
+    if args.is_empty() {
+        return Ok(message.author.id);
+    }
+
+    let arg = args.single::<String>()?;
+    parse_mention(arg)
+        .map(|id| UserId(id))
+        .ok_or_else(|| CommandError::from("Error parsing mention"))
+}
