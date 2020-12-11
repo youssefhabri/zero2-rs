8e2fa2e8930d342cfcdfc2aad0c7795ac31977f4
Youssef Habri <youssef.habri@gmail.com>
2020-12-11 23:20:15 +0100

    Add graceful shutdown of shards

--- src/client.rs
+++ src/client.rs
@@ -39,6 +39,15 @@ impl Zero2Client {
             data.insert::<PaginationContainer>(Arc::new(RwLock::new(HashMap::new())));
         }
 
+        let shard_manager = client.shard_manager.clone();
+        
+        tokio::spawn(async move {
+            tokio::signal::ctrl_c()
+                .await
+                .expect("Could not register ctrl+c handler");
+            shard_manager.lock().await.shutdown_all().await;
+        });
+
         Zero2Client { client }
     }
 
